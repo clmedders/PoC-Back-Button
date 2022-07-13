559ef58ffc025299dfe51602fcbdc0c20f67acb2
Christian Medders <christian.medders@bixal.com>
2022-07-13 09:32:13 -0400

    checkpoint for meeting on friday

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("contact-info");
+  var x = document.getElementsByClassName("menu");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
